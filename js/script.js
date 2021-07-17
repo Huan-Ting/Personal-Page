ee580f6b3c433c00c53b26b4053dbda15470a21f
huantingchen <vvvv00006huan@gmail.com>
2021-07-16 22:19:45 -0400

    second

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let items=document.querySelectorAll(".slider div");
 let tabs=document.querySelectorAll(".tab div");
 
+
 let currentIndex=0;
 function slide(items){
     for(let i=0; i<items.length ;i++){
@@ -8,8 +9,7 @@ function slide(items){
     }
     items[currentIndex].setAttribute("style","display:block;");
     changeTabColor(tabs);
-
-
+    hidePre_Nxt_Button();
 }
 
 
@@ -30,20 +30,43 @@ function changeTabColor(tabs){
     for(let i=0; i<tabs.length ;i++){
         if(i==currentIndex){
             tabs[currentIndex].setAttribute("style","color: red;");
-            tabs[currentIndex].setAttribute("style","background-color: blue;");
+            tabs[currentIndex].setAttribute("style","background-color: #161616;");
         }else{
             tabs[i].setAttribute("style","color: #E4E4E4;");
-            tabs[i].setAttribute("style","background-color: #1E2233;");
+            tabs[i].setAttribute("style","background-color: #2F385D;");
         }
     }
-
-
-
-
-    tabs[currentIndex].setAttribute("style","color: #d85418;");
-    tabs[currentIndex].setAttribute("style","background-color: #020202;");
 }
 
+function hidePre_Nxt_Button(){
+    if(currentIndex==0){
+        $(".pre-btn").attr("style","display:none;");
+        $(".nxt-btn").attr("style","display:block;");
+    }else if(currentIndex==items.length-1){
+        $(".nxt-btn").attr("style","display:none;");
+        $(".pre-btn").attr("style","display:block;");
+    }else{
+        $(".nxt-btn").attr("style","display:block;");
+        $(".pre-btn").attr("style","display:block;");
+    }
+}
+
+/*
+function hidePre_Nxt_Button(slideButton){
+    let pBtn=slideButton[0]
+    let nBtn=slideButton[1]
+    if(currentIndex==0){
+        pBtn.setAttribute("style","display:none;");
+        nBtn.setAttribute("style","display:block;");
+    }else if(currentIndex==items.length){
+        nBtn.setAttribute("style","display:none;");
+        pBtn.setAttribute("style","display:block;");
+    }else{
+        pBtn.setAttribute("style","display:block;");
+        nBtn.setAttribute("style","display:block;");
+    };
+}
+*/
 function go_pre(){}
 function go_nxt(){}
 
